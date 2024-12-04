25141762aeaf36537550f304a91bc202defd52a8
Andromeda75 <23090610@alumno.utmetropolitana.edu.mx>
2024-12-04 03:35:42 -0600

    index.js Modificado

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
 =========================================================
 * Material Dashboard 2 React - v2.2.0
@@ -32,15 +31,3 @@ root.render(
     </MaterialUIControllerProvider>
   </BrowserRouter>
 );
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
-
->>>>>>> 2afde8ff358892109cc11ce1c48312e7a8109fe1
